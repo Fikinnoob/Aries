35a4af4bcb2a989db0b63cbb0c3e0c603b492354
Fikinnoob <nahuelojedao98@gmail.com>
2025-07-29 21:57:53 -0300

    Ultimos cambios

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: import.meta.env.VITE_TU_API_KEY,
+  authDomain: import.meta.env.VITE_TU_DOMINIO,
+  projectId: import.meta.env.VITE_TU_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_TU_BUCKET,
+  messagingSenderId: import.meta.env.VITE_TU_SENDER_ID,
+  appId: import.meta.env.VITE_TU_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
